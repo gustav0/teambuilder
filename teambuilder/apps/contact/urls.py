043d38cf7e153faff86d863683c9a52bb7a448c0
Miguel Ferrer <ingferrermiguel@gmail.com>
2014-02-22 17:22:53 -0430

    Fix . gitignore Miguel

--- teambuilder/apps/contact/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('teambuilder.apps.contact.views',
-    url(r'contact/$', 'contact', name='contact'),
-    url(r'contact/thanks/$', 'thanks', name='thanks'),
-)
