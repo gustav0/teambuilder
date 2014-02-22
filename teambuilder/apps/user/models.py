f8f90e77c4c5dbe9a05954d895fe121cd098a241
Gustavo Saavedra <gustavo.saavedra@inverdata.com>
2014-02-22 17:56:44 -0430

    intento1

---
+++ teambuilder/apps/user/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError('The user must have an email')
+        user = self.model(email=self.normalize_email(email),)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_admin = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    email = models.EmailField(verbose_name='Email', unique=True, db_index=True)
+    first_name = models.CharField(verbose_name='First Name', max_length=30)
+    last_name = models.CharField(verbose_name='Last Name', max_length=30)
+    in_game_name =  models.CharField(verbose_name='In Game Name', max_length=30)
+    current_league =  models.CharField(verbose_name='Current League', max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    register_date = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    class Meta:
+        app_label='user'
+        db_table = 'user'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['-register_date',]
+
+    def __unicode__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.first_name+" "+self.last_name
---
+++ teambuilder/apps/user/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('teambuilder.apps.user.views',
+    url(r'profile/$', 'profile', name='profile'),
+    url(r'register/$', 'register', name='register'),
+)
