43cbd3865cfaae117f9e8beb345292243a7677b2
Gustavo Saavedra <gustavo.saavedra@inverdata.com>
2014-02-22 02:52:07 -0430

    Registro arreglado

--- teambuilder/apps/user/forms.py
+++ teambuilder/apps/user/forms.py
@@ -19,10 +19,3 @@ class registerForm(forms.ModelForm):
             raise forms.ValidationError("Los passwords no coinciden")
         return password2
 
-    def save(self, commit=True):
-        user = super(UserCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
-
