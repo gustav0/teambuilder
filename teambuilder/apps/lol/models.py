0c3c9529508b7828506d2fd34c9eaacc1013926e
Gustavo Saavedra <tavito.286@gmail.com>
2014-02-26 17:36:59 -0430

    Pantalla de champion y script para actualizar champions

--- teambuilder/apps/lol/models.py
+++ teambuilder/apps/lol/models.py
@@ -12,18 +12,19 @@ class Champion(models.Model):
     magic_rank = models.IntegerField(verbose_name='Magic level', max_length=2)
     attack_rank = models.IntegerField(verbose_name='Attack level', max_length=2)
     defense_rank = models.IntegerField(verbose_name='Defense level', max_length=2)
+    free_play = models.BooleanField(verbose_name='Free to play')
     image = models.ImageField(verbose_name='Image', upload_to='teambuilder/app/lol/static/img/champion/')
 
     objects = ChampionManager()
 
-    REQUIRED_FIELDS = ['name', 'riot_id']
+    REQUIRED_FIELDS = ['name', 'riot_id', 'free_play']
 
     class Meta:
         app_label='lol'
         db_table = 'champion'
         verbose_name = 'Champion'
         verbose_name_plural = 'Champions'
-        ordering = ['name',]
+        ordering = ['-free_play','name',]
 
     def __unicode__(self):
         return self.name
--- teambuilder/apps/lol/views.py
+++ teambuilder/apps/lol/views.py
@@ -14,11 +14,3 @@ def champions(request):
     ctx = {'title':title, 'champions':champions}
     return render_to_response('lol/champions.html', ctx, context_instance=RequestContext(request))
 
-def update_champions(request):
-    requestJson = json.loads(_requests.get("https://prod.api.pvp.net/api/lol/lan/v1.1/champion?api_key=cc22a2c0-35d0-4bef-b178-deb4e925120a").text)
-
-    for champion in requestJson['champions']:
-        champ = Champion.objects.create(name=champion['name'], riot_id=champion['id'], difficulty_rank=champion['difficultyRank'], magic_rank=champion['magicRank'], attack_rank=champion['attackRank'], defense_rank=champion['defenseRank'], image="/static/img/champion/%s_Splash_0_e.jpg" % champion['name'])
-        champ.save()
-    return None
-
