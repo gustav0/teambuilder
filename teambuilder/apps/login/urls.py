905a5b5674a033d95c1689beeeec3eb16dd711d0
Gustavo Saavedra <tavito.286@gmail.com>
2014-02-28 23:11:18 -0430

    Cambios y arreglos en el recuperar password. Deberia funcionar bien ahora.

--- teambuilder/apps/login/urls.py
+++ teambuilder/apps/login/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('',
-    url(r'^login/$', 'teambuilder.apps.login.views.login', name='login'),
-    url(r'^logout$', 'teambuilder.apps.login.views.logout', name='logout'),
-    url(r'^user/password/reset/$', 'django.contrib.auth.views.password_reset', {'post_reset_redirect' : '/user/password/reset/done/'}, name="password_reset"),
-    url(r'^user/password/reset/done/$', 'django.contrib.auth.views.password_reset_done'),
-    url(r'^user/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'post_reset_redirect' : '/user/password/done/'}),
-    url(r'^user/password/done/$', 'django.contrib.auth.views.password_reset_complete'),
+urlpatterns = patterns('teambuilder.apps.login.views',
+    url(r'^login/$', 'login', name='login'),
+    url(r'^logout$', 'logout', name='logout'),
+    url(r'^user/password/reset/$', 'password_reset', {'post_reset_redirect' : '/user/password/reset/done/'}, name="password_reset"),
+    url(r'^user/password/reset/done/$', 'password_reset_done'),
+    url(r'^user/password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', 'password_reset_confirm', {'post_reset_redirect' : '/user/password/done/'}, name='password_reset_confirm'),
+    url(r'^user/password/done/$', 'password_reset_complete'),
 )
--- teambuilder/apps/login/views.py
+++ teambuilder/apps/login/views.py
@@ -1,9 +1,14 @@
 from django.contrib.auth import authenticate, login as _login, logout as _logout
+from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm
+from django.contrib.auth.tokens import default_token_generator
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.decorators.cache import never_cache
+from django.contrib.auth.views import password_reset as _password_reset, password_reset_done as _password_reset_done,\
+    password_reset_confirm as _password_reset_confirm, password_reset_complete as _password_reset_complete
 from teambuilder.apps.login.forms import loginForm
-
 
 
 def login(request):
@@ -13,10 +18,7 @@ def login(request):
             email = form.cleaned_data['username']
             password = form.cleaned_data['password']
             user = authenticate(username=email, password=password)
-
-            print email + ' ' + password
-
-            if user and user.is_active:
+            if user is not None and user.is_active:
                 _login(request, user)
                 return HttpResponseRedirect('/profile/')
             else:
@@ -31,3 +33,25 @@ def login(request):
 def logout(request):
     _logout(request)
     return HttpResponseRedirect('/')
+
+def password_reset(request, is_admin_site=False, post_reset_redirect=None, from_email=None, current_app=None,extra_context=None ):
+    return _password_reset(request, is_admin_site, 'login/password_reset_form.html', 'login/password_reset_email.html',
+        'login/password_reset_subject.txt',
+        PasswordResetForm,
+        default_token_generator,
+        post_reset_redirect,
+        from_email,
+        current_app,
+        extra_context)
+
+def password_reset_done(request):
+    return _password_reset_done(request, 'login/password_reset_done.html')
+
+@sensitive_post_parameters()
+@never_cache
+def password_reset_confirm(request, uidb64=None, token=None, post_reset_redirect=None, current_app=None, extra_context=None):
+    return _password_reset_confirm(request, uidb64, token, 'login/password_reset_confirm.html', default_token_generator,
+        SetPasswordForm, post_reset_redirect, current_app, extra_context)
+
+def password_reset_complete(request, current_app=None, extra_context=None):
+    return _password_reset_complete(request,'login/password_reset_complete.html')
